8d881b570248c95376f9670d0091e400e156cbaf
isaacnanino <108468072+isaacnanino@users.noreply.github.com>
2024-05-20 03:24:15 -0700

    trail

---
+++ Template - Birthday/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+
+ /*  Preloader    */
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function() {
+    preloader.classList.add("loaded");
+    this.document.body.classList.add("loaded");
+})
+
